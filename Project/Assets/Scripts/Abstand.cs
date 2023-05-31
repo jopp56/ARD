18ca5f1e129bc8b4352df3bfc8c80f45ce0f1f81
Joshua Schoppe <78209080+jopp56@users.noreply.github.com>
2023-05-31 12:52:06 +0200

    prefab corrected and script

--- Project/Assets/Scripts/Abstand.cs
+++ Project/Assets/Scripts/Abstand.cs
@@ -10,18 +10,13 @@ public class Abstand : MonoBehaviour
     void Awake()
     {
         dartscheibe = GameObject.FindWithTag("Dartscheibe");
-       // abstandslinie = GameObject.Find("Abstandslinie");
+
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        //abstandslinie.transform.position = new Vector3(dartscheibe.transform.position.y - 1.73f) + (dartscheibe.transform.position.z - 2.37f);
+    void Start() { 
+    
         abstandslinie.transform.position = new Vector3(0f, 1f, 1f) * dartscheibe.transform.position.y + new Vector3(0f, 1f, 1f) * dartscheibe.transform.position.z - new Vector3(0f, 1.73f, 2.37f);
-        //abstandslinie.transform.position = new Vector3(0f, dartscheibe.transform.position - 1.73f, dartscheibe.transform.position - 2.37f);
-        //abstandslinie.transform.position.x = 0f;
-        //abstandslinie.transform.position.y = dartscheibe.transform.position.y - 1.73f;
-        //abstandslinie.transform.position.z = dartscheibe.transform.position.z - 2.37f;
         Instantiate(abstandslinie, abstandslinie.transform.position, Quaternion.identity);
     }
 
