c44d706c81b25910013868752a81f54f1169ab38
Joshua Schoppe <78209080+jopp56@users.noreply.github.com>
2023-06-19 18:00:26 +0200

    dartpfeil und änderung kibehaviorscript

--- Project/Assets/Scripts/KIBehaviorScript.cs
+++ Project/Assets/Scripts/KIBehaviorScript.cs
@@ -11,15 +11,13 @@ public class KIBehaviorScript : MonoBehaviour
     private int scoreAnzeigeKICount;
     private int scoreAnzeigePlayerCount;
 
-    public TextMeshPro scoreAnzeigeKIText;
+    public GameObject scoreAnzeigeKI;
+    private TextMeshPro scoreAnzeigeKIText;
 
-    public string[] outerNormalRing;
-    public string[] innerNormalRing;
-    public string[] tripleRing;
-    public string[] doubleRing;
-    public string[] center;
-    private string[][] targetListAfterDifficulty;
-    private int targetListAfterDifficultyLength;
+  
+    public string[] targetListAfterDifficulty;
+    //12.0481928% für evenly verteilung auf 83 bei 1000%
+
 
    
     void Start()
@@ -27,12 +25,9 @@ public class KIBehaviorScript : MonoBehaviour
         dartsCount = 0;
         scoreAnzeigeKICount = 310;
 
-        //Target List for difficulty normal probabilty values: outerNormalRing=innerNormalRing=1/3; center=tripleRing=1/12; doubleRing= 1/6 
-        targetListAfterDifficulty = new string[][] { outerNormalRing,outerNormalRing, outerNormalRing, outerNormalRing, innerNormalRing, innerNormalRing, innerNormalRing,innerNormalRing, 
-                                                    center, tripleRing, doubleRing, doubleRing};
-        targetListAfterDifficultyLength = targetListAfterDifficulty.Length;
 
-        scoreAnzeigeKIText = GameObject.Find("ScoreAnzeigeKI").GetComponent<TextMeshPro>();
+
+        scoreAnzeigeKIText = scoreAnzeigeKI.GetComponent<TextMeshPro>();
         scoreAnzeigePlayerCount = ScoreAnzeigeScript.score;
 
     }
@@ -69,37 +64,9 @@ public class KIBehaviorScript : MonoBehaviour
 
     public void KIChooseAndHitTarget()
     {
-        System.Random choosePossibility = new System.Random();
 
         while (true)
         {
-            int choosePossibilityForHitArea = choosePossibility.Next(0, targetListAfterDifficultyLength);
-
-            string[] intendedTargetInHitArea = targetListAfterDifficulty[choosePossibilityForHitArea];
-            int choosePossibilityForIntendedTarget = choosePossibility.Next(0, intendedTargetInHitArea.Length);
-
-            string intendedTargetAsString = intendedTargetInHitArea[choosePossibilityForIntendedTarget];
-            int intendedTargetAsInt = int.Parse(intendedTargetAsString);
-
-            double chooseHitProbability = choosePossibility.NextDouble();
-            double chooseMissProbability = choosePossibility.NextDouble();
-
-            if (chooseHitProbability > chooseMissProbability)
-            {
-                UpdateKIScore(intendedTargetAsInt);
-                return;
-            }
-            else if (chooseHitProbability < chooseMissProbability)
-            {
-                int chooseProbabilityHitAnotherArea = choosePossibility.Next(0, 1);
-
-                if (chooseProbabilityHitAnotherArea == 0)
-                {
-                    UpdateKIScore(0);
-                    continue;
-                }
-            //Hier wählt man einen neuen target allerdings OHNE den intended target
-            }
         }
     }
 
--- Project/build/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
+++ Project/build/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -12,38 +12,110 @@
 
 
 
-// 0x00000001 System.Void Abstand::Awake()
+// 0x00000001 System.Void ScoreAnzeige::Start()
+extern void ScoreAnzeige_Start_mC5A314B2532DE24508775929F5190901FFBB9368 (void);
+// 0x00000002 System.Void ScoreAnzeige::Update()
+extern void ScoreAnzeige_Update_mBC175B8AA935CD5594EE5B7EA733688E9B178220 (void);
+// 0x00000003 System.Void ScoreAnzeige::.ctor()
+extern void ScoreAnzeige__ctor_m8173B7E935FD1235345139A74A27FDC4A3F037E9 (void);
+// 0x00000004 System.Void Abstand::Awake()
 extern void Abstand_Awake_mFB29563B554436D79F14E2FD7F27FF57EF7D7603 (void);
-// 0x00000002 System.Void Abstand::Start()
+// 0x00000005 System.Void Abstand::Start()
 extern void Abstand_Start_mACC02FFA843608DF8343CFDD62834112E4DB513D (void);
-// 0x00000003 System.Void Abstand::Update()
+// 0x00000006 System.Void Abstand::Update()
 extern void Abstand_Update_mBC1148989EF965BE9E726696F99520414A4A7C18 (void);
-// 0x00000004 System.Void Abstand::.ctor()
+// 0x00000007 System.Void Abstand::.ctor()
 extern void Abstand__ctor_m05C729EEE9FE391A3D19E449BF605607BD0B2C94 (void);
-// 0x00000005 System.Void MenuScript::Awake()
+// 0x00000008 System.Void KIBehaviorScript::Start()
+extern void KIBehaviorScript_Start_mFD4DED5F56ACB2FCBDDB8553B4F2D860A27FD975 (void);
+// 0x00000009 System.Void KIBehaviorScript::Update()
+extern void KIBehaviorScript_Update_m7D991534CC2DD4A62849EE287FCAEF85469980E8 (void);
+// 0x0000000A System.Void KIBehaviorScript::KIRun()
+extern void KIBehaviorScript_KIRun_m849E40377A98BA81012C1A12D0D3A8ACA5D612D8 (void);
+// 0x0000000B System.Void KIBehaviorScript::KIChooseAndHitTarget()
+extern void KIBehaviorScript_KIChooseAndHitTarget_m31D06D069A2EC36CAABA1C27C9AEFED697D12766 (void);
+// 0x0000000C System.Void KIBehaviorScript::KIWaitForPlayerToFinishRound()
+extern void KIBehaviorScript_KIWaitForPlayerToFinishRound_mB647FC09A76A59160651A4BE26CA2157D36FA865 (void);
+// 0x0000000D System.Void KIBehaviorScript::KIChooseAndHitFinishTargets()
+extern void KIBehaviorScript_KIChooseAndHitFinishTargets_m9D04F5A66DACCAA7415782EBD93C24795170B8C8 (void);
+// 0x0000000E System.Void KIBehaviorScript::EndGameSequence()
+extern void KIBehaviorScript_EndGameSequence_m8DD1273B9A3065B16E30EE037339916380E4175D (void);
+// 0x0000000F System.Void KIBehaviorScript::UpdateKIScore(System.Int32)
+extern void KIBehaviorScript_UpdateKIScore_m6C1B089D8E1995C2E7AFBEFB2030BD68D80722A8 (void);
+// 0x00000010 System.Void KIBehaviorScript::.ctor()
+extern void KIBehaviorScript__ctor_mB27608755CAB46FAC4FD96DECA340F3139B544AB (void);
+// 0x00000011 System.Void MenuScript::Awake()
 extern void MenuScript_Awake_m237748B3CD408272A89FA6040C4B1894D38DCDBF (void);
-// 0x00000006 System.Void MenuScript::Start()
+// 0x00000012 System.Void MenuScript::Start()
 extern void MenuScript_Start_m8C843F2A54A5C8116CC175C71C202BACBE173A0C (void);
-// 0x00000007 System.Void MenuScript::Update()
+// 0x00000013 System.Void MenuScript::Update()
 extern void MenuScript_Update_m7F6F2AA220CF46BAB2F638E8A37BC9E23E88C7FF (void);
-// 0x00000008 System.Void MenuScript::ClickToRestart()
+// 0x00000014 System.Void MenuScript::ClickToRestart()
 extern void MenuScript_ClickToRestart_mAEB315B55091B06456DE7E69C5F500DAF98A3E09 (void);
-// 0x00000009 System.Void MenuScript::CloseMenu()
+// 0x00000015 System.Void MenuScript::CloseMenu()
 extern void MenuScript_CloseMenu_m8245BB415D7A23D5F8702EFA24F5DB234AC6D946 (void);
-// 0x0000000A System.Void MenuScript::ToogleMenu()
+// 0x00000016 System.Void MenuScript::ToogleMenu()
 extern void MenuScript_ToogleMenu_mC536E197E304CD3F8358D5B8F805EF7FE10680E8 (void);
-// 0x0000000B System.Void MenuScript::.ctor()
+// 0x00000017 System.Void MenuScript::.ctor()
 extern void MenuScript__ctor_mCDAA51C5C929C62E7082872DA581F913C3E66EDC (void);
-// 0x0000000C System.String VuforiaLicense::GetLicenseKey()
+// 0x00000018 System.Void MinusOne::Awake()
+extern void MinusOne_Awake_m46470BC7BF25778D8BF3A51E5D65A3A331B78311 (void);
+// 0x00000019 System.Void MinusOne::ButtonPressed()
+extern void MinusOne_ButtonPressed_m91545FD68B2B07C90743E42B692983681A4B89D6 (void);
+// 0x0000001A System.Void MinusOne::.ctor()
+extern void MinusOne__ctor_mA2FC19B9CD3CE158472D0B0120F8C4EDD5400A2E (void);
+// 0x0000001B System.Void MinusTwo::Awake()
+extern void MinusTwo_Awake_m48819A481158A1C1F9616F956C46AA2B1D0E8671 (void);
+// 0x0000001C System.Void MinusTwo::ButtonPressed()
+extern void MinusTwo_ButtonPressed_m99238A67458550004EEC1275D3A01CECEA4977ED (void);
+// 0x0000001D System.Void MinusTwo::.ctor()
+extern void MinusTwo__ctor_m75AA727F7FDD6E21EF378F74B7A721DAE2275475 (void);
+// 0x0000001E System.Void ScoreAnzeigeScript::Awake()
+extern void ScoreAnzeigeScript_Awake_mB58EFCA582D61916BCAE7E4CAA1406A52AFF105E (void);
+// 0x0000001F System.Void ScoreAnzeigeScript::Start()
+extern void ScoreAnzeigeScript_Start_mB6EC210F804140162BA9972FD0A28D3941E22127 (void);
+// 0x00000020 System.Void ScoreAnzeigeScript::changeScore()
+extern void ScoreAnzeigeScript_changeScore_mCC61C7B102A4E997C064D609075A13878450D910 (void);
+// 0x00000021 System.Void ScoreAnzeigeScript::.ctor()
+extern void ScoreAnzeigeScript__ctor_m1FB69B3173F6D070B3E1DE289238ECCEE94AC6F4 (void);
+// 0x00000022 System.Void ScoreAnzeigeScript::.cctor()
+extern void ScoreAnzeigeScript__cctor_m0099BD319F435C14AE1585847DE8FB83EFAB30FD (void);
+// 0x00000023 System.Void ScoreEingabeScript::Start()
+extern void ScoreEingabeScript_Start_m53C8DC11B7112FD6036B941F1CF33A2A611EF7EA (void);
+// 0x00000024 System.Void ScoreEingabeScript::Update()
+extern void ScoreEingabeScript_Update_mF2F49D9294DF115A3286B8A6379F60DDCED416D4 (void);
+// 0x00000025 System.Void ScoreEingabeScript::.ctor()
+extern void ScoreEingabeScript__ctor_mEC3A16B14EB71F1768E26E19D23F8C43033331CE (void);
+// 0x00000026 System.Void StartMenuScript::Start()
+extern void StartMenuScript_Start_m8C900DC0893E9E1E49C917E7E7B8EAD6C73B0FA7 (void);
+// 0x00000027 System.Void StartMenuScript::Update()
+extern void StartMenuScript_Update_m5617193A8EA04F32CC7EFAEDF9503E2948FCCAFD (void);
+// 0x00000028 System.Void StartMenuScript::ClickOnSinglePlayerButton()
+extern void StartMenuScript_ClickOnSinglePlayerButton_m6BC44249E71CC9B984163944F0AB829EB2F34D2F (void);
+// 0x00000029 System.Void StartMenuScript::.ctor()
+extern void StartMenuScript__ctor_mB6F3477CA7BC1C9799DB734E012B23282DDC4ADA (void);
+// 0x0000002A System.String VuforiaLicense::GetLicenseKey()
 extern void VuforiaLicense_GetLicenseKey_m8E6E03709CDB6968425A93FB9D016DA6AFC6C91B (void);
-// 0x0000000D System.Void VuforiaLicense::.ctor()
+// 0x0000002B System.Void VuforiaLicense::.ctor()
 extern void VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B (void);
-static Il2CppMethodPointer s_methodPointers[13] = 
+static Il2CppMethodPointer s_methodPointers[43] = 
 {
+	ScoreAnzeige_Start_mC5A314B2532DE24508775929F5190901FFBB9368,
+	ScoreAnzeige_Update_mBC175B8AA935CD5594EE5B7EA733688E9B178220,
+	ScoreAnzeige__ctor_m8173B7E935FD1235345139A74A27FDC4A3F037E9,
 	Abstand_Awake_mFB29563B554436D79F14E2FD7F27FF57EF7D7603,
 	Abstand_Start_mACC02FFA843608DF8343CFDD62834112E4DB513D,
 	Abstand_Update_mBC1148989EF965BE9E726696F99520414A4A7C18,
 	Abstand__ctor_m05C729EEE9FE391A3D19E449BF605607BD0B2C94,
+	KIBehaviorScript_Start_mFD4DED5F56ACB2FCBDDB8553B4F2D860A27FD975,
+	KIBehaviorScript_Update_m7D991534CC2DD4A62849EE287FCAEF85469980E8,
+	KIBehaviorScript_KIRun_m849E40377A98BA81012C1A12D0D3A8ACA5D612D8,
+	KIBehaviorScript_KIChooseAndHitTarget_m31D06D069A2EC36CAABA1C27C9AEFED697D12766,
+	KIBehaviorScript_KIWaitForPlayerToFinishRound_mB647FC09A76A59160651A4BE26CA2157D36FA865,
+	KIBehaviorScript_KIChooseAndHitFinishTargets_m9D04F5A66DACCAA7415782EBD93C24795170B8C8,
+	KIBehaviorScript_EndGameSequence_m8DD1273B9A3065B16E30EE037339916380E4175D,
+	KIBehaviorScript_UpdateKIScore_m6C1B089D8E1995C2E7AFBEFB2030BD68D80722A8,
+	KIBehaviorScript__ctor_mB27608755CAB46FAC4FD96DECA340F3139B544AB,
 	MenuScript_Awake_m237748B3CD408272A89FA6040C4B1894D38DCDBF,
 	MenuScript_Start_m8C843F2A54A5C8116CC175C71C202BACBE173A0C,
 	MenuScript_Update_m7F6F2AA220CF46BAB2F638E8A37BC9E23E88C7FF,
@@ -51,15 +123,63 @@ static Il2CppMethodPointer s_methodPointers[13] =
 	MenuScript_CloseMenu_m8245BB415D7A23D5F8702EFA24F5DB234AC6D946,
 	MenuScript_ToogleMenu_mC536E197E304CD3F8358D5B8F805EF7FE10680E8,
 	MenuScript__ctor_mCDAA51C5C929C62E7082872DA581F913C3E66EDC,
+	MinusOne_Awake_m46470BC7BF25778D8BF3A51E5D65A3A331B78311,
+	MinusOne_ButtonPressed_m91545FD68B2B07C90743E42B692983681A4B89D6,
+	MinusOne__ctor_mA2FC19B9CD3CE158472D0B0120F8C4EDD5400A2E,
+	MinusTwo_Awake_m48819A481158A1C1F9616F956C46AA2B1D0E8671,
+	MinusTwo_ButtonPressed_m99238A67458550004EEC1275D3A01CECEA4977ED,
+	MinusTwo__ctor_m75AA727F7FDD6E21EF378F74B7A721DAE2275475,
+	ScoreAnzeigeScript_Awake_mB58EFCA582D61916BCAE7E4CAA1406A52AFF105E,
+	ScoreAnzeigeScript_Start_mB6EC210F804140162BA9972FD0A28D3941E22127,
+	ScoreAnzeigeScript_changeScore_mCC61C7B102A4E997C064D609075A13878450D910,
+	ScoreAnzeigeScript__ctor_m1FB69B3173F6D070B3E1DE289238ECCEE94AC6F4,
+	ScoreAnzeigeScript__cctor_m0099BD319F435C14AE1585847DE8FB83EFAB30FD,
+	ScoreEingabeScript_Start_m53C8DC11B7112FD6036B941F1CF33A2A611EF7EA,
+	ScoreEingabeScript_Update_mF2F49D9294DF115A3286B8A6379F60DDCED416D4,
+	ScoreEingabeScript__ctor_mEC3A16B14EB71F1768E26E19D23F8C43033331CE,
+	StartMenuScript_Start_m8C900DC0893E9E1E49C917E7E7B8EAD6C73B0FA7,
+	StartMenuScript_Update_m5617193A8EA04F32CC7EFAEDF9503E2948FCCAFD,
+	StartMenuScript_ClickOnSinglePlayerButton_m6BC44249E71CC9B984163944F0AB829EB2F34D2F,
+	StartMenuScript__ctor_mB6F3477CA7BC1C9799DB734E012B23282DDC4ADA,
 	VuforiaLicense_GetLicenseKey_m8E6E03709CDB6968425A93FB9D016DA6AFC6C91B,
 	VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B,
 };
-static const int32_t s_InvokerIndices[13] = 
+static const int32_t s_InvokerIndices[43] = 
 {
 	9130,
 	9130,
 	9130,
 	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	7183,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	9130,
+	13527,
 	9130,
 	9130,
 	9130,
@@ -74,7 +194,7 @@ IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
 const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
 {
 	"Assembly-CSharp.dll",
-	13,
+	43,
 	s_methodPointers,
 	0,
 	NULL,
