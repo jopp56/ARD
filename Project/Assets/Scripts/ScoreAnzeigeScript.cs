95b0403314bf1430d1b398ab37c12d8fe968fb26
sodoe <sofia.doerfler@stud.uni-due.de>
2023-06-25 15:11:49 +0200

    ScoreAnzeige FinishText

--- Project/Assets/Scripts/ScoreAnzeigeScript.cs
+++ Project/Assets/Scripts/ScoreAnzeigeScript.cs
@@ -10,6 +10,7 @@ public class ScoreAnzeigeScript : MonoBehaviour
     public static ScoreAnzeigeScript instance;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI finishText;
 
     public static int score = 301;
     public int eingabesumme;
@@ -29,10 +30,27 @@ public class ScoreAnzeigeScript : MonoBehaviour
     public void changeScore()
     {
         eingabesumme = int.Parse(ScoreEingabeScript.scorearray[0]) + int.Parse(ScoreEingabeScript.scorearray[1]) + int.Parse(ScoreEingabeScript.scorearray[2]);
-        score -= eingabesumme;
-        scoreText.text = "Dein Score:\n" + score.ToString() + " / 301";
-        ScoreEingabeScript.scorearray[0] = "Wurf 1";
-        ScoreEingabeScript.scorearray[1] = "Wurf 2";
-        ScoreEingabeScript.scorearray[2] = "Wurf 3";
+        if (eingabesumme < score)
+        {
+            score -= eingabesumme;
+            scoreText.text = "Dein Score:\n" + score.ToString() + " / 301";
+            ScoreEingabeScript.scorearray[0] = "Wurf 1";
+            ScoreEingabeScript.scorearray[1] = "Wurf 2";
+            ScoreEingabeScript.scorearray[2] = "Wurf 3";
+        } else if (eingabesumme > score)
+        {
+            scoreText.text = "Dein Score:\n" + score.ToString() + " / 301";
+            ScoreEingabeScript.scorearray[0] = "Wurf 1";
+            ScoreEingabeScript.scorearray[1] = "Wurf 2";
+            ScoreEingabeScript.scorearray[2] = "Wurf 3";
+        } 
+        else if (eingabesumme == score) {
+            score -= eingabesumme;
+            scoreText.text = "Dein Score:\n" + score.ToString() + " / 301";
+            ScoreEingabeScript.scorearray[0] = "Wurf 1";
+            ScoreEingabeScript.scorearray[1] = "Wurf 2";
+            ScoreEingabeScript.scorearray[2] = "Wurf 3";
+            finishText.text = "FINISHED!";
+        }
     }
 }
