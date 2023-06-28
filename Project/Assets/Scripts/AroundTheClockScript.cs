4d6abca4596209e5bb91908bb21f1aca080f630e
sodoe <sofia.doerfler@stud.uni-due.de>
2023-06-28 13:04:21 +0200

    Script Around the Clock Training

--- Project/Assets/Scripts/AroundTheClockScript.cs
+++ Project/Assets/Scripts/AroundTheClockScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -7,12 +8,17 @@ using TMPro;
 public class AroundTheClockScript : MonoBehaviour
 {
     public TextMeshProUGUI levelText;
+    public TextMeshProUGUI lebenText;
+    public TextMeshProUGUI endeText;
     public int feld;
+    public int leben;
 
     void Start()
     {
         feld = 1;
+        leben = 3;
         levelText.text = "Triff Feld " + feld.ToString() + "!";
+        lebenText.text = "Du hast noch " + leben.ToString() + " Leben";
     }
 
     void Update()
@@ -20,4 +26,28 @@ public class AroundTheClockScript : MonoBehaviour
         
     }
 
+    public void ButtonGetroffen()
+    {
+        feld += 1;
+        levelText.text = "Triff Feld " + feld.ToString() + "!";
+        if (feld == 20)
+        {
+            endeText.text = "Du hast das Training erfolreich abgeschlossen!";
+        }
+    }
+
+    public void ButtonVerfehlt()
+    {
+        leben -= 1;
+        lebenText.text = "Du hast noch " + leben.ToString() + " Leben";
+
+        if (leben == 0)
+        {
+            feld -= 1;
+            levelText.text = "Triff Feld " + feld.ToString() + "!";
+            leben = 3;
+            lebenText.text = "Du hast noch " + leben.ToString() + " Leben";
+        }
+        
+    }
 }
