7a69a8251ef0b9fc4802fd6ef88a72b6adf7d867
Joshua Schoppe <78209080+jopp56@users.noreply.github.com>
2023-07-02 15:16:07 +0200

    startmenü erstellt

--- Project/Assets/Scripts/StartMenuScript.cs
+++ Project/Assets/Scripts/StartMenuScript.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class StartMenuScript : MonoBehaviour
 {
     public GameObject singlePlayerButton;
+    public GameObject trainingButton1;
+    public GameObject trainingButton2;
+    public GameObject trainingButton3;
+    public GameObject kiButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,4 +26,25 @@ public class StartMenuScript : MonoBehaviour
     {
         SceneManager.LoadScene("DartScene", LoadSceneMode.Single);
     }
+
+    public void ClickOnTrainingButton1()
+    {
+        SceneManager.LoadScene("Level1FinishWegeScene", LoadSceneMode.Single);
+    }
+
+    public void ClickOnTrainingButton2()
+    {
+        SceneManager.LoadScene("Level2Scene", LoadSceneMode.Single);
+    }
+
+    public void ClickOnTrainingButton3()
+    {
+        SceneManager.LoadScene("Level3RandomFelderScene", LoadSceneMode.Single);
+    }
+
+    public void ClickOnKIButton()
+    {
+        SceneManager.LoadScene("KIDartScene", LoadSceneMode.Single);
+    }
+
 }
---
+++ Project/Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWRPTzNkTC8vLy8vQUFBQm1iSWpSZ3RjOUVRSHJFQWVBUFBMcVNFYmJOVmx0MVFMUHZVbStUQU4va1NSZU1IRmFHd0dYaGptdzRDbzJ5REl6TDJSUGV4OThIRkNXRWFWTDRCbEpBYVMyRGZlcXB5cVpSVytpMEpreCtwVFBPSDZnRG82WUZBR29FbkROcDBITnhsUTR3SW5lVUVXTmZyZHZvUmVzdDRxN0VlZ2wxOGJObzJsSlRYTzB0cnpjOUZlcWIzWm9MRG5wOUdwR0NGTlFNOXh5R3hjVkRzRGtaSzZ5cS9lUTNBODNNNDFzcUQ4eXJjMklvWFF1ZUhvK1hoekV3dFkzWk5ja25oMWJtWllwUUhpemhUb3NQdzdCNHd2V2VzUjNheW9pc3lpeTJZenY4TGdJVnBKcFk3blcwR0RVaTN2d3dmbm4vSW90M1l4QWkyTm4rRmx3MWpnZ05HTVBsbG5MM0JORHpGSG1UUXU5MTRYeXRCa3lnWnQ=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
