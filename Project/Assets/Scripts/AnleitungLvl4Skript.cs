f6be2462321fc92e78321368f547a27e3de95d49
Natalie0801 <natalie.scholtyschik@gmail.com>
2023-07-13 19:20:17 +0200

    Lvl 4 Anleitung hinzugefügt

---
+++ Project/Assets/Scripts/AnleitungLvl4Skript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class AnleitungLvl4Skript : MonoBehaviour
+{
+    public TextMeshProUGUI anleitungText;
+    // Start is called before the first frame update
+    void Start()
+    { }
+
+    public void ButtonAnleitung()
+    {
+        anleitungText.text = "Bei diesem Spielmodus müssen die Spieler versuchen, die Zahlen auf dem Dartboard der Reihe nach zu treffen, beginnend mit der 1 und endet mit der 20.";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
